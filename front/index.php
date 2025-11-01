07d8733bcde3d1a58badc4665a14fcda9977d101
Lisandro Zapata <zlisandro5@gmail.com>
2025-10-31 20:20:04 -0500

    Se estructura la primera pagina segun el diseño propuesto

--- front/index.php
+++ front/index.php
@@ -1,11 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TRUEKE | Iniciar sesión</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <title>TRUEKE | Intercambia tus cosas por otras que necesites</title>
+    <style>
+        h1,
+        h2,
+        h3 {
+            color: var(--primary-color);
+            text-align: center;
+            margin-top: 15px;
+            margin-bottom: 15px;
+        }
+    </style>
 </head>
+
 <body>
-    
+    <main>
+        <div class="container-center">
+            <div class="c_icon">
+                <img src="../assets/image.png" alt="">
+            </div>
+            <div>
+                <h3>
+                    Intercambia tus cosas por otras que necesites
+                </h3>
+            </div>
+            <div class="div_100 margin-top-50">
+                <a href="login.php" class="a-especial">
+                    INICIAR SESIÓN
+                </a>
+                <a href="register.php" class="a-especial">
+                    REGISTRARSE
+                </a>
+            </div>
+        </div>
+    </main>
+
 </body>
+
 </html>
