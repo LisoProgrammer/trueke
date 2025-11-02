7a0d0254fd1749ed06f8d90fcf36b49e320a2c4e
Lisandro Zapata <zlisandro5@gmail.com>
2025-11-01 19:52:33 -0500

    Se inicializa el dashboard con el header

---
+++ front/views/dashboard.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/css/style.css">
+    <title>TRUEKE | Dashboard</title>
+</head>
+
+<body>
+    <?php
+    include "../../setting.php";
+    include "modules/header.php";
+    ?>
+    <main>
+        
+    </main>
+
+</body>
+
+</html>
