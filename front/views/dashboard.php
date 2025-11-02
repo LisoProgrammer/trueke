3e688d8bd6eef18343a644d9ad0b616ab8fcdd14
Lisandro Zapata <zlisandro5@gmail.com>
2025-11-02 14:43:53 -0500

    Maquetación del dashboard y distribución de tarjetas de publicacion

--- front/views/dashboard.php
+++ front/views/dashboard.php
@@ -6,6 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../assets/css/style.css">
     <title>TRUEKE | Dashboard</title>
+    <style>
+        h1,
+        h2,
+        h3 {
+            color: #000;
+        }
+    </style>
 </head>
 
 <body>
@@ -14,7 +21,143 @@
     include "modules/header.php";
     ?>
     <main>
-        
+        <h2>¡Hola Lisandro!, tienes 1 solicitud(es) de trueque</h2>
+        <div class="div-group">
+            <div class="card_dash green">
+                <div>
+                    <span>1</span>
+                    <span><img src="/trueke/assets/icons/clip.png" alt=""></span>
+                </div>
+                <span>Pendientes</span>
+            </div>
+            <div class="card_dash red">
+                <div>
+                    <span>1</span>
+                    <span><img src="/trueke/assets/icons/info.png" alt=""></span>
+                </div>
+                <span>Cancelados</span>
+            </div>
+            <div class="card_dash yellow">
+                <div>
+                    <span>4.5</span>
+                    <span><img src="/trueke/assets/icons/star.png" alt=""></span>
+                </div>
+                <span>Tu calificación</span>
+            </div>
+            <div class="card_dash blue">
+                <div>
+                    <span>5</span>
+                    <span><img src="/trueke/assets/icons/trending.png" alt=""></span>
+                </div>
+                <span>Trueques hechos</span>
+            </div>
+        </div>
+        <h2>Tus publicaciones</h2>
+
+        <div class="layout-move-items">
+            <!-- Botón izquierda -->
+            <button class="btn-move prev">
+                <img src="/trueke/assets/icons/left.png" alt="">
+            </button>
+
+            <!-- Contenedor de tarjetas -->
+            <div class="cards-wrapper">
+                <div class="card_item">
+                    <div class="flex">
+                        <div class="flex">
+                            <img src="/trueke/assets/icons/arrow_right.png" alt="">
+                            <div class="flex column">
+                                <span>Lisandro Z...</span>
+                                <span>Hace 3 minutos</span>
+                            </div>
+                        </div>
+
+                        <div>
+                            <span>8</span>
+                            <span>Clicks</span>
+                        </div>
+                    </div>
+
+                    <div class="img">
+                        <img src="/trueke/assets/samples/4335.jpg" alt="">
+                    </div>
+
+                    <div class="flex tools">
+                        <button>Eliminar publicación</button>
+                        <button>
+                            <span class="num_buble">1</span>
+                            <img src="/trueke/assets/icons/gift.png" alt="">
+                        </button>
+                    </div>
+                </div>
+
+                <div class="card_item">
+                    <div class="flex">
+                        <div class="flex">
+                            <img src="/trueke/assets/icons/arrow_right.png" alt="">
+                            <div class="flex column">
+                                <span>Lisandro Z...</span>
+                                <span>Hace 3 minutos</span>
+                            </div>
+                        </div>
+
+                        <div>
+                            <span>8</span>
+                            <span>Clicks</span>
+                        </div>
+                    </div>
+
+                    <div class="img">
+                        <img src="/trueke/assets/samples/4335.jpg" alt="">
+                    </div>
+
+                    <div class="flex tools">
+                        <button>Eliminar publicación</button>
+                        <button>
+                            <span class="num_buble">1</span>
+                            <img src="/trueke/assets/icons/gift.png" alt="">
+                        </button>
+                    </div>
+                </div>
+
+                <div class="card_item">
+                    <div class="flex">
+                        <div class="flex">
+                            <img src="/trueke/assets/icons/arrow_right.png" alt="">
+                            <div class="flex column">
+                                <span>Lisandro Z...</span>
+                                <span>Hace 3 minutos</span>
+                            </div>
+                        </div>
+
+                        <div>
+                            <span>8</span>
+                            <span>Clicks</span>
+                        </div>
+                    </div>
+
+                    <div class="img">
+                        <img src="/trueke/assets/samples/4335.jpg" alt="">
+                    </div>
+
+                    <div class="flex tools">
+                        <button>Eliminar publicación</button>
+                        <button>
+                            <span class="num_buble">1</span>
+                            <img src="/trueke/assets/icons/gift.png" alt="">
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Botón derecha -->
+            <button class="btn-move next">
+                <img src="/trueke/assets/icons/right.png" alt="">
+            </button>
+        </div>
+
+
+        <h2>Sugerencias para tí</h2>
     </main>
 
 </body>
