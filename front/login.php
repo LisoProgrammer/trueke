56dcf407c85de3c226bfb50888321c2aa7a28af2
Lisandro Zapata <zlisandro5@gmail.com>
2025-11-05 21:37:35 -0500

    Ahora el formulario se redirige al modulo de autenticación, una vez enviadas las credenciales

--- front/login.php
+++ front/login.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/css/style.css">
     <title>INICIAR SESIÓN - TRUEKE</title>
-    
+
 </head>
+
 <body>
     <main>
         <div class="container-center">
@@ -14,9 +16,14 @@
                 <img src="../assets/image.png" alt="Logo TRUEKE">
             </div>
             <h1>Iniciar sesión </h1>
-            <form class="registro-form" autocomplete="off" onsubmit="return false;">
+            <form class="registro-form" autocomplete="off" onsubmit="return false;" action="/trueke/back/auth.php">
                 <p>¡Bienvenido! Inicia sesión para empezar a intercambiar cosas.</p>
-
+                <div class="msg-error" id="correoError" style="display:none;">
+                    <span class="icon-error">❌</span> El correo no es válido o no pertenece al dominio <b>@utb.edu.co</b>.
+                </div>
+                <div class="msg-valid" id="correoValido" style="display:none;">
+                    <span class="icon-valid">✔️</span> El correo es válido.
+                </div>
                 <div class="input-group">
                     <span class="input-icon"><img src="../assets/icons/mail.png" alt=""></span>
                     <input type="email" id="correo" placeholder="example@utb.edu.co" required style="text-transform: none;" oninput="validarCorreo()">
@@ -26,7 +33,7 @@
                     <span class="input-icon"><img src="../assets/icons/key.png" alt=""></span>
                     <input type="password" id="pass1" placeholder="Contraseña" minlength="8" required>
                 </div>
-                
+
                 <span id="password_message"></span>
                 <label class="mostrar-pass">
                     <input type="checkbox" onclick="mostrarContrasena()"> Mostrar contraseña
@@ -38,7 +45,7 @@
         <br>
     </main>
     <script src="js/login.js"></script>
-    
-</body>
-</html>
 
+</body>
+
+</html>
