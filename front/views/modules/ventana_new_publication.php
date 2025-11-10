38531b287df2aa22ca05ea3bf3118a473b346f4f
Javier Mercado <javiermercado772@gmail.com>
2025-11-10 18:49:05 -0500

    Se pre-maquetó la ventana de nueva publicacion

---
+++ front/views/modules/ventana_new_publication.php
@@ -0,0 +1,12 @@
+<div class='capa_principal_new_publication closed' id='capa_principal_new_publication'>
+    <div class='window_new_publication closed' id='window_new_publication'>
+        <header class='flex'>
+            <div>
+                <h2>Publicar un nuevo articulo para intercambiar</h2>
+            </div>
+            <button id="close_window_new_publication">
+                <img src="/trueke/assets/icons/close.png" alt="">
+            </button>
+        </header>
+    </div>
+</div>
