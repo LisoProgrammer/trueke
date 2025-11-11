d4a859a42179e0772fa68dd9c248d161affc5de3
Lisandro Zapata <zlisandro5@gmail.com>
2025-11-11 12:07:07 -0500

    Se agregaron los nuevos samples

--- back/database/inserts.sql
+++ back/database/inserts.sql
@@ -6,13 +6,13 @@ INSERT INTO `usuario` (`id`, `primer_nombre`, `primer_apellido`, `foto_perfil`, 
 
 
 INSERT INTO publicacion (id_autor, titulo, descripcion, imagen, fecha, hora, visualizaciones) 
-VALUES (1, 'Cambio de libros de ciencia', 'Ofrezco cambiar libros de ciencia por novelas, interesados escribir.', 'libros.jpg', '2025-11-10', '18:00:00', 15);
+VALUES (1, 'Cambio de libros de ciencia', 'Ofrezco cambiar libros de ciencia por novelas, interesados escribir.', 'libro.jpg', '2025-11-10', '18:00:00', 15);
 
 INSERT INTO publicacion (id_autor, titulo, descripcion, imagen, fecha, hora, visualizaciones)
-VALUES (1, 'Trueque de ropa', 'Cambio ropa en buen estado por zapatos deportivos, talla M.', 'ropa.jpg', '2025-11-10', '18:30:00', 20);
+VALUES (1, 'Trueque de ropa', 'Cambio bata en buen estado por zapatos deportivos, talla M.', 'bata.png', '2025-11-10', '18:30:00', 20);
 
 INSERT INTO publicacion (id_autor, titulo, descripcion, imagen, fecha, hora, visualizaciones)
-VALUES (1, 'Intercambio de juegos', 'Ofrezco juegos de mesa a cambio de videojuegos de PS4.', 'juegos.jpg', '2025-11-10', '19:00:00', 10);
+VALUES (1, 'Intercambio de juegos', 'Ofrezco juegos de mesa a cambio de videojuegos de PS4.', 'juego.png', '2025-11-10', '19:00:00', 10);
 
 INSERT INTO publicacion (id_autor, titulo, descripcion, imagen, fecha, hora, visualizaciones)
-VALUES (1, 'Herramientas por utensilios', 'Busco intercambiar herramientas eléctricas por utensilios de cocina variados.', 'herramientas.jpg', '2025-11-10', '19:30:00', 25);
+VALUES (1, 'Herramientas por utensilios', 'Busco intercambiar botas por utensilios de cocina variados.', 'botas.png', '2025-11-10', '19:30:00', 25);
