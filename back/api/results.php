0547368d458e39bb01fe1ceb677c874f3f857dbc
Lisandro Zapata <zlisandro5@gmail.com>
2025-11-11 12:06:49 -0500

    Se hace un JOIN para obtener el nombre del autor

--- back/api/results.php
+++ back/api/results.php
@@ -1,16 +1,17 @@
 <?php
-require __DIR__."/../connection.php";
-if(isset($_GET["buscar"])){
-    $sql_busqueda=$con->prepare("SELECT `id_publicacion`, `id_autor`, `titulo`, `descripcion`, `imagen`, `fecha`, `hora`, `visualizaciones` FROM `publicacion` WHERE titulo LIKE CONCAT('%',?,'%') OR descripcion LIKE CONCAT('%',?,'%') ORDER BY id_publicacion DESC");
-    if($sql_busqueda){
-        $sql_busqueda->bind_param("ss",$_GET["buscar"],$_GET["buscar"]);
+require __DIR__ . "/../connection.php";
+if (isset($_GET["buscar"])) {
+    $sql_busqueda = $con->prepare("SELECT `id_publicacion`, usuario.primer_nombre as primer_nombre, usuario.primer_apellido as primer_apellido, `titulo`, `descripcion`, `imagen`, `fecha`, `hora`, `visualizaciones` FROM `publicacion` JOIN usuario ON usuario.id = publicacion.id_autor WHERE LOWER(titulo) LIKE CONCAT('%',?,'%') OR LOWER(descripcion) LIKE CONCAT('%',?,'%') ORDER BY id_publicacion DESC");
+    if ($sql_busqueda) {
+        $input_lower = strtolower($_GET["buscar"]);
+        $sql_busqueda->bind_param("ss", $input_lower, $input_lower);
         $sql_busqueda->execute();
-        $result=$sql_busqueda->get_result();
-        $all_result=[];
-        while($row=$result->fetch_assoc()){
+        $result = $sql_busqueda->get_result();
+        $all_result = [];
+        while ($row = $result->fetch_assoc()) {
 
             $all_result[$row["id_publicacion"]] = [
-                "id_autor" => $row["id_autor"],
+                "autor" => $row["primer_nombre"] . " " . $row["primer_apellido"],
                 "titulo" => $row["titulo"],
                 "descripcion" => $row["descripcion"],
                 "imagen" => $row["imagen"],
@@ -20,8 +21,7 @@ if(isset($_GET["buscar"])){
             ];
         }
         echo "<script>";
-        print_r("let results = ".json_encode($all_result));
+        print_r("let results = " . json_encode($all_result));
         echo "</script>";
     }
-        
 }
