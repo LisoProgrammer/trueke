d5afc8b24bf65875ac1bb836284c78a2c3796697
Lisandro Zapata <zlisandro5@gmail.com>
2025-11-10 20:29:39 -0500

    Se solucionan los errores con la conexión a la base de datos y se filtran el titulo y la descripcon buscando coincidencias con la entrada

--- back/api/results.php
+++ back/api/results.php
@@ -1,25 +1,25 @@
 <?php
-require "../connecion.php";
+require __DIR__."/../connection.php";
 if(isset($_GET["buscar"])){
-    $sql_busqueda=$con->prepare("SELECT `id_publicacion`, `id_autor`, `titulo`, `descripcion`, `imagen`, `fecha`, `hora`, `visualizaciones` FROM `publicacion` WHERE titulo=? ");
+    $sql_busqueda=$con->prepare("SELECT `id_publicacion`, `id_autor`, `titulo`, `descripcion`, `imagen`, `fecha`, `hora`, `visualizaciones` FROM `publicacion` WHERE titulo LIKE CONCAT('%',?,'%') OR descripcion LIKE CONCAT('%',?,'%') ORDER BY id_publicacion DESC");
     if($sql_busqueda){
-        $sql_busqueda->bind_parem("s",$_GET["buscar"]);
+        $sql_busqueda->bind_param("ss",$_GET["buscar"],$_GET["buscar"]);
         $sql_busqueda->execute();
         $result=$sql_busqueda->get_result();
         $all_result=[];
         while($row=$result->fetch_assoc()){
-
-            $all_result[$row["id_publicacion"]]=[
-                "id_autor"=>$row["id_autor"],
-                "titulo"=>$row["titulo"],
-                "descripcion"=>$row["descripcion"],
-                "imagen"=>$row["imagen"],
-                "fecha"=>$row["fecha"],
-                "hora "=>$row["hora"],
-                "visualizaciones"=>$row["visualizaciones"]
+            
+            $all_result[$row["id_publicacion"]] = [
+                "id_autor" => $row["id_autor"],
+                "titulo" => $row["titulo"],
+                "descripcion" => $row["descripcion"],
+                "imagen" => $row["imagen"],
+                "fecha" => $row["fecha"],
+                "hora " => $row["hora"],
+                "visualizaciones" => $row["visualizaciones"]
             ];
         }
-        print_r(all_result);
+        print_r($all_result);
     }
         
 }
