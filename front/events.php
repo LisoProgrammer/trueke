369e100ffffc35ec8c1339c66981bdf8717e2aa3
Lisandro Zapata <zlisandro5@gmail.com>
2025-11-12 17:21:22 -0500

    Se agregaron los mensajes de error y exito para las publicaciones

--- front/events.php
+++ front/events.php
@@ -4,11 +4,14 @@ if (isset($_GET["code_msg"])) {
     /**
      * Codigos 100: relacionados con el registro de usuario
      * Codigos 200: relacionados con el inicio de sesión
+     * Codigos 300: relacionados con la publicacion de articulos o servicios
      */
     $alerts = [
         100 => ["title" => "¡Yupi!", "msg" => "Usuario creado correctamente", "icon" => "success"],
         110 => ["title" => "¡Oops!", "msg" => "El usuario ya existe. Intenta iniciar sesión", "icon" => "error"],
-        210 => ["title" => "¡Oops!", "msg" => "La contraseña es incorrecta. Intenta nuevamente.", "icon" => "error"]
+        210 => ["title" => "¡Oops!", "msg" => "La contraseña es incorrecta. Intenta nuevamente.", "icon" => "error"],
+        300 => ["title" => "¡Yupi!", "msg" => "Se creó tu publicación con éxito.", "icon" => "success"],
+        310 => ["title" => "¡Oops!", "msg" => "Ocurrió un error al intentar crear tu publicación.", "icon" => "error"]
     ];
 
     // Validar que el código exista en el array
