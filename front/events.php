7b1dfe7d1c68427b2af004d2d3f668ee2263629c
Lisandro Zapata <zlisandro5@gmail.com>
2025-11-15 11:01:49 -0500

    Agregado los eventos relacioados con la publicacion de ofertas

--- front/events.php
+++ front/events.php
@@ -5,13 +5,16 @@ if (isset($_GET["code_msg"])) {
      * Codigos 100: relacionados con el registro de usuario
      * Codigos 200: relacionados con el inicio de sesión
      * Codigos 300: relacionados con la publicacion de articulos o servicios
+     * Codigos 400: relacionados con la publicacion de ofertas de trueque
      */
     $alerts = [
         100 => ["title" => "¡Yupi!", "msg" => "Usuario creado correctamente", "icon" => "success"],
         110 => ["title" => "¡Oops!", "msg" => "El usuario ya existe. Intenta iniciar sesión", "icon" => "error"],
         210 => ["title" => "¡Oops!", "msg" => "La contraseña es incorrecta. Intenta nuevamente.", "icon" => "error"],
         300 => ["title" => "¡Yupi!", "msg" => "Se creó tu publicación con éxito.", "icon" => "success"],
-        310 => ["title" => "¡Oops!", "msg" => "Ocurrió un error al intentar crear tu publicación.", "icon" => "error"]
+        310 => ["title" => "¡Oops!", "msg" => "Ocurrió un error al intentar crear tu publicación.", "icon" => "error"],
+        400 => ["title" => "¡Yupi!", "msg" => "Se publicó tu oferta con éxito.", "icon" => "success"],
+        410 => ["title" => "¡Oops!", "msg" => "Ocurrió un error al intentar publicar tu oferta.", "icon" => "error"]
     ];
 
     // Validar que el código exista en el array
