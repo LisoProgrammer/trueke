f8e5c50eba1617bc56f3b3a41dfa3a34db7a20d6
Lisandro Zapata <zlisandro5@gmail.com>
2025-11-14 23:13:54 -0500

    Para evitar que un usuario se pueda auto solicitar trueques

--- back/api/results.php
+++ back/api/results.php
@@ -1,10 +1,11 @@
 <?php
 require __DIR__ . "/../connection.php";
 if (isset($_GET["buscar"])) {
-    $sql_busqueda = $con->prepare("SELECT `id_publicacion`, usuario.primer_nombre as primer_nombre, usuario.primer_apellido as primer_apellido, `titulo`, `descripcion`, `imagen`, `fecha`, `hora`, `visualizaciones` FROM `publicacion` JOIN usuario ON usuario.id = publicacion.id_autor WHERE LOWER(titulo) LIKE CONCAT('%',?,'%') OR LOWER(descripcion) LIKE CONCAT('%',?,'%') ORDER BY id_publicacion DESC");
+    $id_user = $_SESSION["user"]["id"];
+    $sql_busqueda = $con->prepare("SELECT `id_publicacion`, usuario.primer_nombre as primer_nombre, usuario.primer_apellido as primer_apellido, `titulo`, `descripcion`, `imagen`, `fecha`, `hora`, `visualizaciones` FROM `publicacion` JOIN usuario ON usuario.id = publicacion.id_autor WHERE usuario.id != ? AND LOWER(titulo) LIKE CONCAT('%',?,'%') OR LOWER(descripcion) LIKE CONCAT('%',?,'%') ORDER BY id_publicacion DESC");
     if ($sql_busqueda) {
         $input_lower = strtolower($_GET["buscar"]);
-        $sql_busqueda->bind_param("ss", $input_lower, $input_lower);
+        $sql_busqueda->bind_param("iss", $id_user, $input_lower, $input_lower);
         $sql_busqueda->execute();
         $result = $sql_busqueda->get_result();
         $all_result = [];
