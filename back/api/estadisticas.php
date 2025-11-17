0392946a737336bb0fac641860e6f58415102cc4
Javier Mercado <javiermercado772@gmail.com>
2025-11-16 22:49:10 -0500

    Se obtienen las estadisticas y se empaquetan en un json para el fronted

--- back/api/estadisticas.php
+++ back/api/estadisticas.php
@@ -1,16 +1,23 @@
 <?php
-session_start();
-require "../connection.php";
-echo "\nAqui se cargaran las estadisticas<br>";
+require __DIR__."/../connection.php";
 $id_user = $_SESSION["user"]["id"];
-echo "id del usuario: ".$id_user;
+#echo "id del usuario: ".$id_user;
+
+$respuesta = [
+    "pendientes" => 0, 
+    "calificacion" => 0,
+    "views" => 0,
+    "truekes_hechos" => 0
+]; 
+
 $sqlget_pendiente = $con->prepare("SELECT count(id_solicitado_a) AS n_filas FROM solicitado_a WHERE id_usuario1 = ?");
 if($sqlget_pendiente){
     $sqlget_pendiente->bind_param("i",$id_user);
     $sqlget_pendiente->execute();
     $sqlget_pendiente->bind_result($n_filas);
     $sqlget_pendiente->fetch();
-    echo "<br>pendientes:".$n_filas;
+    #echo "<br>pendientes:".$n_filas;
+    $respuesta["pendientes"] = $n_filas;
     $sqlget_pendiente->close();
 }
 
@@ -20,6 +27,33 @@ if($sqlget_calificacion){
     $sqlget_calificacion->execute();
     $sqlget_calificacion->bind_result($calificacion);
     $sqlget_calificacion->fetch();
-    echo "<br>Calificacion:".$calificacion;
+    #echo "<br>Calificacion:".$calificacion;
+    $respuesta["calificacion"] = $calificacion;
     $sqlget_calificacion->close();
 }
+
+$sqlget_view = $con->prepare("SELECT sum(visualizaciones) AS views FROM publicacion WHERE id_autor = ?");
+if($sqlget_view){
+    $sqlget_view->bind_param("i",$id_user);
+    $sqlget_view->execute();
+    $sqlget_view->bind_result($view);
+    $sqlget_view->fetch();
+    #echo "<br>View:".$view;
+    $respuesta["views"] = $view;
+    $sqlget_view->close();
+}
+
+$sqlget_hechos = $con->prepare("SELECT count(hecho) AS hechos FROM trueke WHERE id_usuario = ?");
+if($sqlget_hechos){
+    $sqlget_hechos->bind_param("i",$id_user);
+    $sqlget_hechos->execute();
+    $sqlget_hechos->bind_result($hechos);
+    $sqlget_hechos->fetch();
+    #echo "<br>Hechos:".$hechos;
+    $respuesta["truekes_hechos"] = $hechos;
+    $sqlget_hechos->close();
+}
+
+echo "<script>";
+print_r("let estadisticas = ".json_encode($respuesta));
+echo "</script>";
