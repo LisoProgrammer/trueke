54ca74c4667238224e526d531c844719cb879d1a
Javier Mercado <javiermercado772@gmail.com>
2025-11-16 22:54:39 -0500

    Error solucionado, faltaban los inner.text en las demas variables

--- front/js/load_stadistics.js
+++ front/js/load_stadistics.js
@@ -2,8 +2,9 @@ let pendiente_span = document.getElementById("pendiente_span")
 let vistas_span = document.getElementById("vistas_span");
 let calificacion_span = document.getElementById("calificacion_span");
 let truekes_hechos_span = document.getElementById("truekes_hechos_span");
+
 console.log(estadisticas.calificacion)
 pendiente_span.innerText = estadisticas.pendientes;
-calificacion_span = estadisticas.calificacion;
-vistas_span = estadisticas.views;
-truekes_hechos_span = estadisticas.truekes_hechos;
+calificacion_span.innerText = estadisticas.calificacion;
+vistas_span.innerText = estadisticas.views;
+truekes_hechos_span.innerText = estadisticas.truekes_hechos;
