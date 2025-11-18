747d2f3ed83a8b19c3612ef622c629e4fbbfd1a8
Lisandro Zapata <zlisandro5@gmail.com>
2025-11-18 16:18:40 -0500

    Se agregan nuevos mensajes y se limpia la URL del codigo del mensaje sin eliminar los demas parametros

--- front/events.php
+++ front/events.php
@@ -1,12 +1,7 @@
 <?php
 if (isset($_GET["code_msg"])) {
     $code_msg = intval($_GET["code_msg"]);
-    /**
-     * Codigos 100: relacionados con el registro de usuario
-     * Codigos 200: relacionados con el inicio de sesión
-     * Codigos 300: relacionados con la publicacion de articulos o servicios
-     * Codigos 400: relacionados con la publicacion de ofertas de trueque
-     */
+
     $alerts = [
         100 => ["title" => "¡Yupi!", "msg" => "Usuario creado correctamente", "icon" => "success"],
         110 => ["title" => "¡Oops!", "msg" => "El usuario ya existe. Intenta iniciar sesión", "icon" => "error"],
@@ -14,11 +9,15 @@ if (isset($_GET["code_msg"])) {
         300 => ["title" => "¡Yupi!", "msg" => "Se creó tu publicación con éxito.", "icon" => "success"],
         310 => ["title" => "¡Oops!", "msg" => "Ocurrió un error al intentar crear tu publicación.", "icon" => "error"],
         400 => ["title" => "¡Yupi!", "msg" => "Se publicó tu oferta con éxito.", "icon" => "success"],
-        410 => ["title" => "¡Oops!", "msg" => "Ocurrió un error al intentar publicar tu oferta.", "icon" => "error"]
+        410 => ["title" => "¡Oops!", "msg" => "Ocurrió un error al intentar publicar tu oferta.", "icon" => "error"],
+        420 => ["title" => "¡Excelente!", "msg" => "Trueque en proceso. Ponte en contacto con oferente.", "icon" => "success"],
+        430 => ["title" => "¡Oops!", "msg" => "Ocurrió un error al intentar aceptar el trueque. Inténtalo nuevamente", "icon" => "error"],
+        440 => ["title" => "¡Listo!", "msg" => "Se ha cancelado el trueque.", "icon" => "success"],
+        450 => ["title" => "¡Listo!", "msg" => "Se ha terminado el trueque.", "icon" => "success"]
     ];
 
-    // Validar que el código exista en el array
     if (isset($alerts[$code_msg])) {
+        echo "<script>let code_msg = ". $code_msg. "</script>";
         ?>
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
         <script>
@@ -32,8 +31,10 @@ if (isset($_GET["code_msg"])) {
                 icon: icon_msg,
                 confirmButtonText: "Aceptar"
             }).then(() => {
-                // Se limpia el parámetro GET para evitar que se repita al recargar
-                window.history.replaceState(null, "", window.location.pathname);
+                // Limpia solo el parámetro code_msg, mantiene otros parámetros
+                const url = new URL(window.location.href);
+                url.searchParams.delete("code_msg");
+                window.history.replaceState({}, document.title, url.toString());
             });
         </script>
         <?php
